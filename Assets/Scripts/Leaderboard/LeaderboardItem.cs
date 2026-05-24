2f544739ab9ae748dbca903d79989dfce1fd8a25
Yan Costode <ycostode@2C7.42angouleme.fr>
2026-05-24 19:59:04 +0200

    add: format score

--- Assets/Scripts/Leaderboard/LeaderboardItem.cs
+++ Assets/Scripts/Leaderboard/LeaderboardItem.cs
@@ -11,12 +11,6 @@ public class LeaderboardItem : MonoBehaviour
     {
         nameText.text = _playerData.playerName;
         timeText.text = _playerData.fTimer.ToString("N0") + "s";
-        scoreText.text = _playerData.score.ToString("N0");
-        /*
-        string _raw = _playerData.score.ToString("e3");
-        string[] _parts = _raw.Split("e");
-        int _power = int.Parse(_parts[1]);
-        scoreText.text = $"{_parts[0]}<size=50%>x</size>10<sup>{_power}</sup>";
-        */
+        scoreText.text = HUD.FormatScore(_playerData.score);
     }
 }
--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -25,15 +25,17 @@ public class HUD : Singleton<HUD>
     public void SetScore(double _score)
     {
         foreach (TMP_Text _text in scoreText)
-            _text.text = $"{_score:N0}";
+            _text.text = FormatScore(_score);
+    }
+
+    public static string FormatScore(double _score)
+    {
+        if (_score < 1000000.0)
+            return $"{_score:N0}";
         
-        /*
         string _raw = _score.ToString("e3");
         string[] _parts = _raw.Split("e");
         int _power = int.Parse(_parts[1]);
-
-        foreach (TMP_Text _text in scoreText)
-            _text.text = $"{_parts[0]}<size=50%>x</size>10<sup>{_power}</sup>";
-            */
+        return $"{_parts[0]}<size=50%>x</size>10<sup>{_power}</sup>";
     }
 }
